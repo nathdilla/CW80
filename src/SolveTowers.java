3372ffd58ab76c072834f3804fc0cc2b72eae634
nxthando <natedillamc@gmail.com>
2020-02-26 11:36:21 -0800

    Initial commit

---
+++ src/SolveTowers.java
@@ -0,0 +1,11 @@
+public class SolveTowers
+{
+    public static void main(String[] args)
+    {
+        TowersOfHanoi towers = new TowersOfHanoi(4);
+
+        towers.solve();
+
+        System.out.println("Nathan Dilla");
+    }
+}
